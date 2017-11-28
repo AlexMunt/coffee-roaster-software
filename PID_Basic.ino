e7a4bcf15bd4fd11fad5a9140951ec92f23b14dd
AlexMunt <32861851+AlexMunt@users.noreply.github.com>
2017-11-28 07:25:40 +0100

    Add files via upload

---
+++ PID_Basic.ino
@@ -0,0 +1,30 @@
+/********************************************************
+ * PID Basic Example
+ * Reading analog input 0 to control analog PWM output 3
+ ********************************************************/
+
+#include <PID_v1.h>  // This is the library
+
+#define PIN_INPUT 0  // The Input, a potenciometer for example, 
+#define PIN_OUTPUT 3 // The PWM Output,a  LED for example 
+
+double Setpoint, Input, Output; //Define Variables of the PID
+double Kp=2, Ki=5, Kd=1;        //The tuning parameters of the PID  
+                                // The PID
+PID myPID(&Input, &Output, &Setpoint, Kp, Ki, Kd, DIRECT);
+
+void setup()
+{
+  Input = analogRead(PIN_INPUT); //initialize the variables we're linked to
+  Setpoint = 100;                // The setpoint value, the reference of the PID
+  myPID.SetMode(AUTOMATIC);      // START ON, turn the PID on
+}
+
+void loop()
+{
+  Input = analogRead(PIN_INPUT);
+  myPID.Compute();
+  analogWrite(PIN_OUTPUT, Output);
+}
+
+
